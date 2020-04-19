b8d47e9b10ca63121d0ff086c1b35434f52f3e07
Tenzing Nyima <ShiYash@Tenzings-Air.attlocal.net>
2020-04-18 21:44:49 -0700

    first commit

---
+++ src/main/java/com/nema/mvc/repositories/BookRepository.java
@@ -0,0 +1,24 @@
+package com.nema.mvc.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import com.nema.mvc.models.Book;
+
+
+
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+	//This method retrieves all the books from the database
+	List<Book> findAll();
+	
+	//this method finds books with descriptions containing the search string
+	List<Book> findByDescriptionContaining(String search);
+	
+	//this method counts how many titles contain a certain string
+	Long countByTitleContaining(String search);
+	
+	//this method deletes a book that starts with a specific title
+	Long deleteByTitleStartingWith(String search);
+}
